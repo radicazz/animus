5dbc27997cb47ed92d8399cfab8469d3892048c1
cazz <vortexshrimp@gmail.com>
2025-09-19 18:10:35 -0700

    Improved docs for logging utilites.

--- src/engine/logger.hxx
+++ src/engine/logger.hxx
@@ -1,11 +1,30 @@
-#pragma once
+/**
+ * @file logger.hxx
+ * @brief Logging utilities for the game engine.
+ *
+ * This file provides various wrappers around SDL's logging functions with two major featuress
+ * added.
+ *
+ * First, these functions use C++20's `std::format` for type-safe, modern string formatting
+ * instead of SDL's C-style `printf` formatting.
+ *
+ * Second, depending on your CMake configuration, logging at different levels can be completely
+ * avoided from the binary to eliminate any runtime overhead when logging is not needed.
+ */
 
-#include "config.hxx"
+#pragma once
 
 #include <format>
 #include <SDL3/SDL.h>
+#include "config.hxx"
 
 namespace engine {
+    /**
+     * @brief Log a formatted message to the standard output with SDL_Log.
+     * @param fmt The format string.
+     * @param args The arguments to format into the string.
+     * @note Prefer using `log_info`, `log_warning`, or `log_error` for level-specific logging.
+     */
     template <class... Args>
     void log_formatted(std::string_view fmt, Args... args) {
         if constexpr (should_log_info || should_log_warnings || should_log_errors) {
@@ -14,6 +33,12 @@ namespace engine {
         }
     }
 
+    /**
+     * @brief Log an informational message.
+     * @param fmt The format string.
+     * @param args The arguments to format into the string.
+     * @note Only compiled if `ENGINE_LOG_INFO` is ON.
+     */
     template <class... Args>
     void log_info(std::string_view fmt, Args... args) {
         if constexpr (should_log_info) {
@@ -21,6 +46,12 @@ namespace engine {
         }
     }
 
+    /**
+     * @brief Log a warning message.
+     * @param fmt The format string.
+     * @param args The arguments to format into the string.
+     * @note Only compiled if `ENGINE_LOG_WARNINGS` is ON.
+     */
     template <class... Args>
     void log_warning(std::string_view fmt, Args... args) {
         if constexpr (should_log_warnings) {
@@ -28,6 +59,12 @@ namespace engine {
         }
     }
 
+    /**
+     * @brief Log an error message.
+     * @param fmt The format string.
+     * @param args The arguments to format into the string.
+     * @note Only compiled if `ENGINE_LOG_ERRORS` is ON.
+     */
     template <class... Args>
     void log_error(std::string_view fmt, Args... args) {
         if constexpr (should_log_errors) {
